7194a09a7ed1f01332058df9e7bf8c5aeae077a5
frowing37 <froward37@gmail.com>
2024-10-29 01:12:39 +0300

    using redis

--- plan/main.go
+++ plan/main.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"fmt"
 	"github.com/dreamsofcode-io/orders-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
